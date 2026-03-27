01fcbc1cc3d5ac8f267f805815785130b8cfe765
Florian Rey <florian.rey38@gmail.com>
2026-03-27 16:18:56 +0100

    fix: changements dans le texte

--- src/dashboard/app.py
+++ src/dashboard/app.py
@@ -269,7 +269,7 @@ elif page == "🎯 Coach":
 elif page == "⚙️ ML Pipeline":
     st.title("⚙️ ML Pipeline")
     st.info(
-        "MLflow disponible sur localhost:5000. " "Les premiers modèles seront trackés en Phase 3."
+        "MLflow disponible sur localhost:5000. " "Les premiers modèles seront trackés plus tard"
     )
     c1, c2 = st.columns(2)
     with c1:
